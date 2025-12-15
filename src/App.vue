dd5c28b26811135bc63fd7d6c9b351aa11356ac2
李翔 <lx3325360@gmail.com>
2025-12-15 15:01:46 +0800

    chore(release): 更新版本号至 2.6.5
    - SHCarrier.exe 升级为 v1.6.1
    
    - 修改 README.md 中的版本徽章为 2.6.5
    - 更新 package.json、package-lock.json、Cargo.toml 和 tauri.conf.json 中的版本号
    - 调整 App.vue 中的默认版本号为 2.6.5
    - 更新用户手册中显示的版本信息为 2.6.5

--- src/App.vue
+++ src/App.vue
@@ -46,7 +46,7 @@ interface ProcessResult {
 }
 
 // 获取应用版本号
-const appVersion = (globalThis as any).__APP_VERSION__ || '2.6.4';
+const appVersion = (globalThis as any).__APP_VERSION__ || '2.6.5';
 
 const selectedFiles = ref<string[]>([]);
 const selectedTool = ref<ToolType>(ToolType.AneuFiler);
